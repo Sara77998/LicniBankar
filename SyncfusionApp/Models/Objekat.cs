8cc41d1b1a607c371a776b1969c665da23ac0eda
SaraC <SaraC@PCPROGRAMER08>
2022-05-17 15:32:50 +0200

    initial commit

---
+++ SyncfusionApp/Models/Objekat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncfusionApp.Models
+{
+    public class Objekat
+    {
+        public int Sifra { get; set; }
+        public string Naziv { get; set; }
+        public string OpisMesto { get; set; }
+        public double Cena { get; set; }
+        public bool Kes { get; set; }
+        public bool Kartica { get; set; }
+        public DateTime Datum { get; set; }
+        public bool Prihod { get; set; }
+        public bool Trosak { get; set; }
+    }
+}
